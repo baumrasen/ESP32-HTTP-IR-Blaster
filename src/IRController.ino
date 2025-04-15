282aa76cf3064dd64e03f33dcb0abe3a22e8d192
baumrasen <baumrasen@gmail.com>
2025-04-15 19:45:21 +0200

    remove mdns

--- src/IRController.ino
+++ src/IRController.ino
@@ -6,7 +6,6 @@
 #include <IRutils.h>
 #include <WiFi.h>
 #include <WiFiManager.h>                                      // https://github.com/tzapu/WiFiManager WiFi Configuration Magic
-#include <ESPmDNS.h>                                      // Useful to access to ESP by hostname.local
 
 #include <ArduinoJson.h>
 #include <WebServer.h>
@@ -24,8 +23,6 @@ const bool getExternalIP = true;                               // Set to false t
 
 const bool getTime = true;                                     // Set to false to disable querying for the time
 const int timeZone = -5;                                       // Timezone (-5 is EST)
-
-const bool enableMDNSServices = true;                          // Use mDNS services, must be enabled for ArduinoOTA
 
 const unsigned int captureBufSize = 1024;                      // Size of the IR capture buffer.
 
@@ -1054,35 +1051,6 @@ void setup() {
   Serial.print("DNS IP: ");
   Serial.println(WiFi.dnsIP().toString());
   Serial.println("URL to send commands: http://" + String(host_name) + ".local:" + port_str);
-
-  if (enableMDNSServices) {
-    // Configure OTA Update
-    ArduinoOTA.setPort(8266);
-    ArduinoOTA.setHostname(host_name);
-    ArduinoOTA.onStart([]() {
-      Serial.println("Start");
-    });
-    ArduinoOTA.onEnd([]() {
-      Serial.println("\nEnd");
-    });
-    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-    });
-    ArduinoOTA.onError([](ota_error_t error) {
-      Serial.printf("Error[%u]: ", error);
-      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-      else if (error == OTA_END_ERROR) Serial.println("End Failed");
-    });
-    ArduinoOTA.begin();
-    Serial.println("ArduinoOTA started");
-
-    // Configure mDNS
-    MDNS.addService("http", "tcp", port); // Announce the ESP as an HTTP service
-    Serial.println("MDNS http service added. Hostname is set to " + String(host_name) + ".local:" + String(port));
-  }
 
   // Configure the server
   server->on("/json", []() { // JSON handler for more complicated IR blaster routines
